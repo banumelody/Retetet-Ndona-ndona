5b2cb3aa29ec6ffe9faafda2a43e8267111a984e
@banumelody <banumelady@gmail.com>
2016-01-21 21:18:44 +0700

    remove unused code

--- Retetet Ndona/ViewController.swift
+++ Retetet Ndona/ViewController.swift
@@ -18,44 +18,10 @@ class ViewController: UIViewController {
     var buttons = [RDButton]()
     
     var audioPlayer:AVAudioPlayer!
-    var maxVol:CGFloat!
-    
-    var audioName = [
-        "train-whistle-01",
-        "motorcycle-start-and-revs-02",
-        "grass-trimmer-1",
-        "bell-ring-01"
-    ]
     var motorTypes = ["A", "B", "C", "D"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-//        button[1] = RDButton()
-        
-        
-
-        let audioFilePath = NSBundle.mainBundle().pathForResource("motorcycle-start-and-revs-02", ofType: "wav")
-        
-        if audioFilePath != nil {
-            
-            let audioFileUrl = NSURL.fileURLWithPath(audioFilePath!)
-            
-            do {
-                try audioPlayer = AVAudioPlayer(contentsOfURL: audioFileUrl, fileTypeHint: nil)
-                audioPlayer.numberOfLoops = -1
-            } catch {
-                print("audio file is not found")
-            }
-            
-            
-        } else {
-            print("audio file is not found")
-        }
-        
-        maxVol = view.bounds.height
-        
     }
     
     var buttonWidth:CGFloat!
@@ -90,14 +56,7 @@ class ViewController: UIViewController {
 //        print(motorTypes[sender.selectedSegmentIndex])
     }
    
-//    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
-//        buttonWidth = (buttonsContainer.bounds.width-40)/4
-//        buttonHeight = buttonsContainer.frame.size.height - 16
-//        for index in 0...3 {
-//            buttons[index].frame = CGRect(x: (CGFloat(index) * (buttonWidth + 8)) + 8, y: 8, width: buttonWidth, height: buttonHeight)
-//        }
-//        
-//    }
+
     
     override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
         buttonWidth = (buttonsContainer.bounds.width-40)/4
