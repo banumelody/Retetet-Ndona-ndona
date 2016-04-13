029b40aafc47b959deaf3785451b1373ac41f176
@banumelody <banumelady@gmail.com>
2016-04-13 20:35:25 +0700

    First commit

--- Retetet Ndona/ViewController.swift
+++ Retetet Ndona/ViewController.swift
@@ -12,7 +12,6 @@ import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet var buttonsContainer: UIView!
-    
     @IBOutlet weak var selector: UISegmentedControl!
     
     var buttons = [RDButton]()
@@ -20,8 +19,17 @@ class ViewController: UIViewController {
     var audioPlayer:AVAudioPlayer!
     var motorTypes = ["A", "B", "C", "D"]
 
+    var backgroundColors = [
+        UIColor(red: 0.643, green: 0.561, blue: 0.212, alpha: 1.00),
+        UIColor(red: 0.157, green: 0.475, blue: 0.251, alpha: 1.00),
+        UIColor(red: 0.643, green: 0.271, blue: 0.212, alpha: 1.00),
+        UIColor(red: 0.235, green: 0.180, blue: 0.443, alpha: 1.00)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = backgroundColors[0]
+        selector.tintColor = backgroundColors[0]
     }
     
     var buttonWidth:CGFloat!
@@ -34,26 +42,22 @@ class ViewController: UIViewController {
         for index in 0...3 {
             
             let button = RDButton(frame: CGRect(x: (CGFloat(index) * (buttonWidth + 8)) + 8, y: 8, width: buttonWidth, height: buttonHeight))
-            //            button.frame = CGRect(x: index * 100, y: 0, width: 100, height: 100)
             button.backgroundColor = UIColor.lightGrayColor()
-//            button.audioName("motor_jay-" + motorTypes[0] + "-" + String(index+1))
-            
+
             buttons.append(button)
             buttons[index].audioName("motor_jay-" + motorTypes[0] + "-" + String(index+1))
             
             buttonsContainer.addSubview(button)
-            
-        }
-        
 
+        }
     }
     @IBAction func selectorValueChanged(sender: AnyObject) {
+        self.view.backgroundColor = backgroundColors[sender.selectedSegmentIndex]
+        selector.tintColor = backgroundColors[sender.selectedSegmentIndex]
         for index in 0...3 {
             buttons[index].setAudioName("motor_jay-" + motorTypes[sender.selectedSegmentIndex] + "-" + String(index+1), index: index)
-//            print("motor_jay-" + motorTypes[sender.selectedSegmentIndex] + "-" + String(index+1))
             buttons[index].audioPlayer.stop()
         }
-//        print(motorTypes[sender.selectedSegmentIndex])
     }
    
 
